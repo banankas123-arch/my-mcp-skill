c6ee1c42633c589015bebdff93094af07b5fb941
Полина Пылева <polinapyleva@MacBook-Pro-Polina.local>
2026-06-09 16:39:42 +0300

    My first MCP server

---
+++ server.py
@@ -0,0 +1,14 @@
+from mcp.server.fastmcp import FastMCP
+import os
+
+mcp = FastMCP("MySkill")
+
+@mcp.tool()
+def secret_calculation(number: int) -> str:
+    """Секретный расчёт"""
+    result = number * 42 + 13
+    return f"Результат: {result}"
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
+    mcp.run(transport="sse")
